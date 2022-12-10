5429f82f3833e0589026e234720b861a5c749730
Pablo Ortigosa <55493443+PabloOQ@users.noreply.github.com>
2022-12-10 16:34:20 +0000

    Initial InputStick implementation

---
+++ src/Android/Services/AutoTypers/InputStick.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Bit.Core.Abstractions;
+using Com.Inputstick.Api.Broadcast;
+
+namespace Bit.Droid.Services.AutoTypers
+{
+    public class InputStick : IAutoTyperService
+    {
+        public void DirectType(String text)
+        {
+            var context = Application.Context;
+            InputStickBroadcast.Type(context,text,"es-ES");
+        }
+    }
+}
---
+++ src/Core/Abstractions/IAutoTyperService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bit.Core.Abstractions
+{
+    public interface IAutoTyperService
+    {
+        // For easy connections without setup
+        void DirectType(String text);
+    }
+}
