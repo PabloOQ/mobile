48e1d23fdac447186e89c901e940790d273437cf
Pablo Ortigosa <55493443+PabloOQ@users.noreply.github.com>
2022-12-16 17:40:35 +0000

    Enum to string

--- src/Android/Services/AutoTypers/InputStick.cs
+++ src/Android/Services/AutoTypers/InputStick.cs
@@ -29,9 +29,64 @@ namespace Bit.Droid.Services.AutoTypers
         {
             switch (layout)
             {
-                case Layout.en_US: 
+                case Layout.fr_BE:
+                    return "fr-BE";
+                case Layout.hr_HR:
+                    return "hr-HR";
+                case Layout.cs_CZ:
+                    return "cs-CZ";
+                case Layout.da_DK:
+                    return "da-DK";
+                case Layout.nl_NL:
+                    return "nl-NL";
+                case Layout.de_CH:
+                    return "de-CH";
+                case Layout.de_DE:
+                    return "de-DE";
+                case Layout.de_DE_MAC:
+                    return "de-DE-MAC";
+                case Layout.el_GR:
+                    return "el-GR";
+                case Layout.en_US_DV:
+                    return "en-US-DV";
+                case Layout.en_GB:
+                    return "en-GB";
+                case Layout.en_US:
                     return "en-US";
-                case Layout.
+                case Layout.en_US_INT:
+                    return "en-US-INT";
+                case Layout.es_ES:
+                    return "es-ES";
+                case Layout.fi_FI:
+                    return "fi-FI";
+                case Layout.fr_CA:
+                    return "fr-CA";
+                case Layout.fr_CH:
+                    return "fr-CH";
+                case Layout.fr_FR:
+                    return "fr-FR";
+                case Layout.he_IL:
+                    return "he-IL";
+                case Layout.hu_HU:
+                    return "hu-HU";
+                case Layout.it_IT:
+                    return "it-IT";
+                case Layout.nb_NO:
+                    return "nb-NO";
+                case Layout.pl_PL:
+                    return "pl-PL";
+                case Layout.pt_BR:
+                    return "pt-BR";
+                case Layout.pt_PT:
+                    return "pt-PT";
+                case Layout.ru_RU:
+                    return "ru-RU";
+                case Layout.sk_SK:
+                    return "sk-SK";
+                case Layout.sv_SE:
+                    return "sv-SE";
+                default:
+                    return null;
             }
         }
     }
