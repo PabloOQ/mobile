ef432f4a167437812070e1ccf876249cf46fee33
Pablo Ortigosa <55493443+PabloOQ@users.noreply.github.com>
2023-02-14 20:02:37 +0000

    Remove comments

--- src/Core/Enums/LayoutType.cs
+++ src/Core/Enums/LayoutType.cs
@@ -6,60 +6,60 @@ namespace Bit.Core.Enums
     public enum LayoutType
     {
         [LocalizableEnum("Czech (Czech Republic)")]
-        cs_CZ       , // Czech (Czech Republic)
+        cs_CZ,
         [LocalizableEnum("Danish (Denmark)")]
-        da_DK       , // Danish (Denmark)
+        da_DK,
         [LocalizableEnum("German (Switzerland)")]
-        de_CH       , // German (Switzerland)
+        de_CH,
         [LocalizableEnum("German (Germany)")]
-        de_DE       , // German (Germany)
+        de_DE,
         [LocalizableEnum("German (Germany), Mac compatible version")]
-        de_DE_MAC   , // German (Germany), Mac compatible version
+        de_DE_MAC,
         [LocalizableEnum("Greek (Greece)")]
-        el_GR       , // Greek (Greece)
+        el_GR,
         [LocalizableEnum("English (United Kingdom)")]
-        en_GB       , // English (United Kingdom)
+        en_GB,
         [LocalizableEnum("English (United States)")]
-        en_US       , // English (United States)
+        en_US,
         [LocalizableEnum("English (United States), Dvorak layout")]
-        en_US_DV    , // English (United States), Dvorak layout
+        en_US_DV,
         [LocalizableEnum("English (United States), International")]
-        en_US_INT   , // English (United States International)
+        en_US_INT,
         [LocalizableEnum("Spanish (Spain)")]
-        es_ES       , // Spanish (Spain)
+        es_ES,
         [LocalizableEnum("Finnish (Finland)")]
-        fi_FI       , // Finnish (Finland)
+        fi_FI,
         [LocalizableEnum("French (Belgian)")]
-        fr_BE       , // French (Belgian)
+        fr_BE,
         [LocalizableEnum("French (Canada)")]
-        fr_CA       , // French (Canada)
+        fr_CA,
         [LocalizableEnum("French (Switzerland)")]
-        fr_CH       , // French (Switzerland)
+        fr_CH,
         [LocalizableEnum("French (France)")]
-        fr_FR       , // French (France)
+        fr_FR,
         [LocalizableEnum("Hebrew (Israel)")]
-        he_IL       , // Hebrew (Israel)
+        he_IL,
         [LocalizableEnum("Croatian (Croatia)")]
-        hr_HR       , // Croatian (Croatia)
+        hr_HR,
         [LocalizableEnum("Hungarian (Hungary)")]
-        hu_HU       , // Hungarian (Hungary)
+        hu_HU,
         [LocalizableEnum("Italian (Italy)")]
-        it_IT       , // Italian (Italy)
+        it_IT,
         [LocalizableEnum("Norwegian, Bokmal (Norway)")]
-        nb_NO       , // Norwegian, Bokmal (Norway)
+        nb_NO,
         [LocalizableEnum("Dutch (Netherlands)")]
-        nl_NL       , // Dutch (Netherlands)
+        nl_NL,
         [LocalizableEnum("Polish (Poland)")]
-        pl_PL       , // Polish (Poland)
+        pl_PL,
         [LocalizableEnum("Portuguese (Brazil)")]
-        pt_BR       , // Portuguese (Brazil)
+        pt_BR,
         [LocalizableEnum("Portuguese (Portugal)")]
-        pt_PT       , // Portuguese (Portugal)
+        pt_PT,
         [LocalizableEnum("Russian (Russia)")]
-        ru_RU       , // Russian (Russia)
+        ru_RU,
         [LocalizableEnum("Slovak (Slovakia)")]
-        sk_SK       , // Slovak (Slovakia)
+        sk_SK,
         [LocalizableEnum("Swedish (Sweden)")]
-        sv_SE       , // Swedish (Sweden)
+        sv_SE,
     }
 }
