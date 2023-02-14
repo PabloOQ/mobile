481c59fe9a1c421c26999df85b4331097b242d77
Pablo Ortigosa <55493443+PabloOQ@users.noreply.github.com>
2023-02-14 20:06:13 +0000

    Added fixed id

--- src/Core/Enums/LayoutType.cs
+++ src/Core/Enums/LayoutType.cs
@@ -6,60 +6,60 @@ namespace Bit.Core.Enums
     public enum LayoutType
     {
         [LocalizableEnum("Czech (Czech Republic)")]
-        cs_CZ,
+        cs_CZ = 0,
         [LocalizableEnum("Danish (Denmark)")]
-        da_DK,
+        da_DK = 1,
         [LocalizableEnum("German (Switzerland)")]
-        de_CH,
+        de_CH = 2,
         [LocalizableEnum("German (Germany)")]
-        de_DE,
+        de_DE = 3,
         [LocalizableEnum("German (Germany), Mac compatible version")]
-        de_DE_MAC,
+        de_DE_MAC = 4,
         [LocalizableEnum("Greek (Greece)")]
-        el_GR,
+        el_GR = 5,
         [LocalizableEnum("English (United Kingdom)")]
-        en_GB,
+        en_GB = 6,
         [LocalizableEnum("English (United States)")]
-        en_US,
+        en_US = 7,
         [LocalizableEnum("English (United States), Dvorak layout")]
-        en_US_DV,
+        en_US_DV = 8,
         [LocalizableEnum("English (United States), International")]
-        en_US_INT,
+        en_US_INT = 9,
         [LocalizableEnum("Spanish (Spain)")]
-        es_ES,
+        es_ES = 10,
         [LocalizableEnum("Finnish (Finland)")]
-        fi_FI,
+        fi_FI = 11,
         [LocalizableEnum("French (Belgian)")]
-        fr_BE,
+        fr_BE = 12,
         [LocalizableEnum("French (Canada)")]
-        fr_CA,
+        fr_CA = 13,
         [LocalizableEnum("French (Switzerland)")]
-        fr_CH,
+        fr_CH = 14,
         [LocalizableEnum("French (France)")]
-        fr_FR,
+        fr_FR = 15,
         [LocalizableEnum("Hebrew (Israel)")]
-        he_IL,
+        he_IL = 16,
         [LocalizableEnum("Croatian (Croatia)")]
-        hr_HR,
+        hr_HR = 17,
         [LocalizableEnum("Hungarian (Hungary)")]
-        hu_HU,
+        hu_HU = 18,
         [LocalizableEnum("Italian (Italy)")]
-        it_IT,
+        it_IT = 19,
         [LocalizableEnum("Norwegian, Bokmal (Norway)")]
-        nb_NO,
+        nb_NO = 20,
         [LocalizableEnum("Dutch (Netherlands)")]
-        nl_NL,
+        nl_NL = 21,
         [LocalizableEnum("Polish (Poland)")]
-        pl_PL,
+        pl_PL = 22,
         [LocalizableEnum("Portuguese (Brazil)")]
-        pt_BR,
+        pt_BR = 23,
         [LocalizableEnum("Portuguese (Portugal)")]
-        pt_PT,
+        pt_PT = 24,
         [LocalizableEnum("Russian (Russia)")]
-        ru_RU,
+        ru_RU = 25,
         [LocalizableEnum("Slovak (Slovakia)")]
-        sk_SK,
+        sk_SK = 26,
         [LocalizableEnum("Swedish (Sweden)")]
-        sv_SE,
+        sv_SE = 27,
     }
 }
