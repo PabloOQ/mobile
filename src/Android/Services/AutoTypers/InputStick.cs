0d94f5440110477abe90447c6925cce156484057
Pablo Ortigosa <55493443+PabloOQ@users.noreply.github.com>
2023-02-16 15:03:53 +0000

    Squash me

--- src/Android/Services/AutoTypers/InputStick.cs
+++ src/Android/Services/AutoTypers/InputStick.cs
@@ -17,80 +17,84 @@ namespace Bit.Droid.Services.AutoTypers
 {
     public class InputStick : IAutoTyper
     {
-        public LayoutType layout { get; set; } = LayoutType.en_US;
-        public int speed { get; set; } = 1;
-        public void Type(String text)
+        List<KeyValuePair<LayoutType, string>> layouts;
+
+        public InputStick()
+        {
+            // Compatible layouts and their values
+            layouts = new List<KeyValuePair<LayoutType, string>>();
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.cs_CZ,      "cs-CZ"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.da_DK,      "da-DK"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.de_CH,      "de-CH"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.de_DE,      "de-DE"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.de_DE_MAC,  "de-DE-MAC" ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.el_GR,      "el-GR"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.en_GB,      "en-GB"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.en_US,      "en-US"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.en_US_DV,   "en-US-DV"  ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.en_US_INT,  "en-US-INT" ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.es_ES,      "es-ES"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.fi_FI,      "fi-FI"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.fr_CA,      "fr-CA"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.fr_CH,      "fr-CH"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.fr_BE,      "fr-BE"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.fr_FR,      "fr-FR"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.he_IL,      "he-IL"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.hr_HR,      "hr-HR"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.hu_HU,      "hu-HU"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.it_IT,      "it-IT"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.nb_NO,      "nb-NO"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.nl_NL,      "nl-NL"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.pl_PL,      "pl-PL"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.pt_BR,      "pt-BR"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.pt_PT,      "pt-PT"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.ru_RU,      "ru-RU"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.sk_SK,      "sk-SK"     ));
+            layouts.Add(new KeyValuePair<LayoutType, string>(LayoutType.sv_SE,      "sv-SE"     ));
+        }
+
+        public void Type(String text, LayoutType layout, int speed)
         {
             var context = Application.Context;
-            InputStickBroadcast.Type(context,text,GetLayoutKey(layout),speed);
+            InputStickBroadcast.Type(context,text,LayoutKey(layout),speed);
         }
 
-        private String GetLayoutKey(LayoutType layout)
+
+        private String LayoutKey(LayoutType layout) => layout switch
         {
-            switch (layout)
-            {
-                case LayoutType.fr_BE:
-                    return "fr-BE";
-                case LayoutType.hr_HR:
-                    return "hr-HR";
-                case LayoutType.cs_CZ:
-                    return "cs-CZ";
-                case LayoutType.da_DK:
-                    return "da-DK";
-                case LayoutType.nl_NL:
-                    return "nl-NL";
-                case LayoutType.de_CH:
-                    return "de-CH";
-                case LayoutType.de_DE:
-                    return "de-DE";
-                case LayoutType.de_DE_MAC:
-                    return "de-DE-MAC";
-                case LayoutType.el_GR:
-                    return "el-GR";
-                case LayoutType.en_US_DV:
-                    return "en-US-DV";
-                case LayoutType.en_GB:
-                    return "en-GB";
-                case LayoutType.en_US:
-                    return "en-US";
-                case LayoutType.en_US_INT:
-                    return "en-US-INT";
-                case LayoutType.es_ES:
-                    return "es-ES";
-                case LayoutType.fi_FI:
-                    return "fi-FI";
-                case LayoutType.fr_CA:
-                    return "fr-CA";
-                case LayoutType.fr_CH:
-                    return "fr-CH";
-                case LayoutType.fr_FR:
-                    return "fr-FR";
-                case LayoutType.he_IL:
-                    return "he-IL";
-                case LayoutType.hu_HU:
-                    return "hu-HU";
-                case LayoutType.it_IT:
-                    return "it-IT";
-                case LayoutType.nb_NO:
-                    return "nb-NO";
-                case LayoutType.pl_PL:
-                    return "pl-PL";
-                case LayoutType.pt_BR:
-                    return "pt-BR";
-                case LayoutType.pt_PT:
-                    return "pt-PT";
-                case LayoutType.ru_RU:
-                    return "ru-RU";
-                case LayoutType.sk_SK:
-                    return "sk-SK";
-                case LayoutType.sv_SE:
-                    return "sv-SE";
-                default:
-                    return null;
-            }
-        }
+            LayoutType.cs_CZ => "cs-CZ",
+            LayoutType.da_DK => "da-DK",
+            LayoutType.de_CH => "de-CH",
+            LayoutType.de_DE => "de-DE",
+            LayoutType.de_DE_MAC => "de-DE-MAC",
+            LayoutType.el_GR => "el-GR",
+            LayoutType.en_GB => "en-GB",
+            LayoutType.en_US => "en-US",
+            LayoutType.en_US_DV => "en-US-DV",
+            LayoutType.en_US_INT => "en-US-INT",
+            LayoutType.es_ES => "es-ES",
+            LayoutType.fi_FI => "fi-FI",
+            LayoutType.fr_CA => "fr-CA",
+            LayoutType.fr_CH => "fr-CH",
+            LayoutType.fr_BE => "fr-BE",
+            LayoutType.fr_FR => "fr-FR",
+            LayoutType.he_IL => "he-IL",
+            LayoutType.hr_HR => "hr-HR",
+            LayoutType.hu_HU => "hu-HU",
+            LayoutType.it_IT => "it-IT",
+            LayoutType.nb_NO => "nb-NO",
+            LayoutType.nl_NL => "nl-NL",
+            LayoutType.pl_PL => "pl-PL",
+            LayoutType.pt_BR => "pt-BR",
+            LayoutType.pt_PT => "pt-PT",
+            LayoutType.ru_RU => "ru-RU",
+            LayoutType.sk_SK => "sk-SK",
+            LayoutType.sv_SE => "sv-SE",
+            _ => null,
+        };
 
-        public List<LayoutType> GetCompatibleLayouts()
+
+        public List<LayoutType> CompatibleLayouts()
         {
             var layouts = new List<LayoutType>();
 
@@ -102,7 +106,7 @@ namespace Bit.Droid.Services.AutoTypers
             layouts.Add(LayoutType.el_GR);
             layouts.Add(LayoutType.en_GB);
             layouts.Add(LayoutType.en_US);
-            layouts.Add(LayoutType.en_US);
+            layouts.Add(LayoutType.en_US_DV);
             layouts.Add(LayoutType.en_US_INT);
             layouts.Add(LayoutType.es_ES);
             layouts.Add(LayoutType.fi_FI);
